8d72f1c54ee89978d1f805dafebcfbc23febf375
Dustin Knox <chilly3@tutanota.com>
2021-04-29 18:08:38 -0500

    Added config example file for database url variable and begin middleware functions

---
+++ express-locallibrary-tutorial/config.example.js
@@ -0,0 +1,7 @@
+/*
+//Copy contents of this file into new file '/express-locallibrary-tutorial/config.js' then uncomment and replace <connection string> with your database url (i.e. 'mongodb+srv://user_name:pass_word.123.mongodb.net/-local_llibrary?retryWrites=true&w=majority')
+
+var mongoDB = <connection string>;
+
+module.exports = mongoDB;
+*/
